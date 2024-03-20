78dee248f9d2052aae3384dcde8c5ac45ee9cab0
yosephdev <yosephbet@gmail.com>
2024-03-20 08:37:00 +0000

    feat: Add example of complex filtering based on skills and experience

--- filter.js
+++ filter.js
@@ -19,3 +19,41 @@ const people = [
 
   const paul = people.filter(p => p.name === "Paul")[0];
   console.log(paul);
+
+  // Complex Filtering
+const students = [
+    {
+      id: 1,
+      name: 'Mark',
+      profession: 'Developer',
+      skills: [
+        { name: 'javascript', yrsExperience: 1 },
+        { name: 'html', yrsExperience: 5 },
+        { name: 'css', yrsExperience: 3 },
+      ]
+    },
+    {
+      id: 2,
+      name: 'Ariel',
+      profession: 'Developer',
+      skills: [
+        { name: 'javascript', yrsExperience: 0 },
+        { name: 'html', yrsExperience: 4 },
+        { name: 'css', yrsExperience: 2 },
+      ]
+    },
+    {
+      id: 3,
+      name: 'Jason',
+      profession: 'Designer',
+      skills: [
+        { name: 'javascript', yrsExperience: 1 },
+        { name: 'html', yrsExperience: 1 },
+        { name: 'css', yrsExperience: 5 },
+      ]
+    },
+  ];
+  const has5yearsExp = skill => skill.yrsExperience >= 5;
+  const hasStrongSkills = student => student.skills.filter(has5yearsExp).length > 0;  
+  const candidates = students.filter(hasStrongSkills)
+  console.log(candidates);
