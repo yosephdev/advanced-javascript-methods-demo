64ff9be69a56b9de3daf6ffeeb17dd7710546abf
yosephdev <yosephbet@gmail.com>
2024-03-20 09:28:13 +0000

    feat: Add example of summing an array of numbers using reduce method

---
+++ reduce.js
@@ -0,0 +1,42 @@
+// Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+
+let sum = nums.reduce((acc, curr) => {
+
+console.log(
+    "Accumlator:", acc,
+    "Current Value:", curr,
+    "Total:", acc + curr
+    )
+    return acc + curr;
+}, 0);
+
+console.log(sum);
+
+const teamMembers = [
+    {
+      name: 'Andrew',
+      profession: 'Developer',
+      yrsExperience: 5
+    },
+    {
+      name: 'Ariel',
+      profession: 'Developer',
+      yrsExperience: 7
+    },
+    {
+      name: 'Michael',
+      profession: 'Designer',
+      yrsExperience: 1
+    },
+    {
+      name: 'Kelly',
+      profession: 'Designer',
+      yrsExperience: 3
+    }
+  ];
+  
+  // Totaling a specific object property
+  
+  
+  // Grouping by a property, and totaling it too
